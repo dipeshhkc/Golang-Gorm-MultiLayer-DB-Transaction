edeab29ba9cf0024113f2057f95d6c2ef142a041
dipesh <kcdipesh429@gmail.com>
2021-04-19 20:17:25 +0545

    feat: setup GIN route setup

---
+++ route/setup.go
@@ -0,0 +1,34 @@
+package route
+
+import (
+	"golang-transaction/controller"
+	"golang-transaction/repository"
+	"golang-transaction/service"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+//SetupRoutes : all the routes are defined here
+func SetupRoutes(db *gorm.DB) {
+	httpRouter := gin.Default()
+
+	userRepository := repository.NewUserRepository(db)
+
+	if err := userRepository.Migrate(); err != nil {
+		log.Fatal("User migrate err", err)
+	}
+	userService := service.NewUserService(userRepository)
+
+	userController := controller.NewUserController(userService)
+
+	users := httpRouter.Group("users")
+
+	users.GET("/", userController.GetAllUser)
+	users.POST("/", userController.AddUser)
+
+	httpRouter.POST("/money-transfer", userController.TransferMoney)
+	httpRouter.Run()
+
+}
