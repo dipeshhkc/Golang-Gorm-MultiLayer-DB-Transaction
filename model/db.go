e84c678e81ffa162343be2509f2ccfdbce1e6e02
dipesh <kcdipesh429@gmail.com>
2021-04-19 20:16:36 +0545

    feat: database setup

---
+++ model/db.go
@@ -0,0 +1,35 @@
+package model
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+//DBConnection -> return db instance
+func DBConnection() (*gorm.DB, error) {
+	USER := "root"
+	PASS := "root"
+	HOST := "localhost"
+	PORT := "3306"
+	DBNAME := "db-transaction"
+
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold: time.Second, // Slow SQL threshold
+			LogLevel:      logger.Info, // Log level
+			Colorful:      true,        // Disable color
+		},
+	)
+
+	url := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", USER, PASS, HOST, PORT, DBNAME)
+
+	return gorm.Open(mysql.Open(url), &gorm.Config{Logger: newLogger})
+
+}
